80fb85d4a324166377ebd20e9f0eef2809278aef
Sukhpreet Sekhon <ersukhpreetsekhon03@gmail.com>
2015-10-24 15:11:12 -0400

    added background,platform, player and created prefab for coins

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    private AudioSource[] _audioSource;
+    private AudioSource _coinSound;
+
+	// Use this for initialization
+	void Start () {
+
+        this._audioSource = gameObject.GetComponents<AudioSource>();
+        this._coinSound = this._audioSource[0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D otherCollider)
+    {
+        if (otherCollider.gameObject.CompareTag("Coin"))
+        {
+            this._coinSound.Play();
+        }
+    }
+}
